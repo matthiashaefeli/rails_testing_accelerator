9589aa73c018e5a25a39d6290b2c9d0d1534edac
Jason Swett <jason@benfranklinlabs.com>
2020-05-08 13:24:18 -0500

    Make it impossible to create a cluck when not signed in.

--- app/controllers/clucks_controller.rb
+++ app/controllers/clucks_controller.rb
@@ -1,25 +1,21 @@
 class ClucksController < ApplicationController
   before_action :set_cluck, only: [:show, :edit, :update, :destroy]
 
-  # GET /clucks
   def index
     @clucks = Cluck.all
   end
 
-  # GET /clucks/1
   def show
   end
 
-  # GET /clucks/new
   def new
+    redirect_to new_user_session_path unless signed_in?
     @cluck = Cluck.new
   end
 
-  # GET /clucks/1/edit
   def edit
   end
 
-  # POST /clucks
   def create
     @cluck = current_user.clucks.new(cluck_params)
 
@@ -30,7 +26,6 @@ class ClucksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clucks/1
   def update
     if @cluck.update(cluck_params)
       redirect_to @cluck, notice: 'Cluck was successfully updated.'
@@ -39,20 +34,18 @@ class ClucksController < ApplicationController
     end
   end
 
-  # DELETE /clucks/1
   def destroy
     @cluck.destroy
     redirect_to clucks_url, notice: 'Cluck was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cluck
-      @cluck = Cluck.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def cluck_params
-      params.require(:cluck).permit(:content, :user_id)
-    end
+  def set_cluck
+    @cluck = Cluck.find(params[:id])
+  end
+
+  def cluck_params
+    params.require(:cluck).permit(:content, :user_id)
+  end
 end
--- spec/system/create_cluck_spec.rb
+++ spec/system/create_cluck_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a cluck', type: :system do
-  before do
-    login_as(FactoryBot.create(:user))
-  end
+  context 'signed in' do
+    before do
+      login_as(FactoryBot.create(:user))
+    end
 
-  context 'valid cluck' do
-    it 'works' do
-      visit new_cluck_path
-      fill_in 'Content', with: 'Hello'
-      click_on 'Create Cluck'
-      expect(page).to have_content('Hello')
+    context 'valid cluck' do
+      it 'works' do
+        visit new_cluck_path
+        fill_in 'Content', with: 'Hello'
+        click_on 'Create Cluck'
+        expect(page).to have_content('Hello')
+      end
+    end
+
+    context 'blank' do
+      it 'gives it a validation error' do
+        visit new_cluck_path
+        fill_in 'Content', with: ''
+        click_on 'Create Cluck'
+        expect(page).to have_content("Content can't be blank")
+      end
     end
   end
 
-  context 'blank' do
-    it 'gives it a validation error' do
+  context 'not signed in' do
+    it 'does not let me view the form' do
       visit new_cluck_path
-      fill_in 'Content', with: ''
-      click_on 'Create Cluck'
-      expect(page).to have_content("Content can't be blank")
+      expect(page).not_to have_content('Content')
     end
   end
 end

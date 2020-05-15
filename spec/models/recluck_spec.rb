90b0f4d35a1f81cf5929ed74f5f24dfa927ab19d
Jason Swett <jason@benfranklinlabs.com>
2020-05-15 16:57:01 -0400

    Make it so cluck can't be reclucked by same user twice.

--- spec/models/recluck_spec.rb
+++ spec/models/recluck_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Recluck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'trying to recluck the same cluck twice' do
+    it 'is not allowed' do
+      first_recluck = FactoryBot.create(:recluck)
+
+      other_recluck = FactoryBot.build(
+        :recluck,
+        cluck: first_recluck.cluck,
+        user: first_recluck.user
+      )
+
+      expect(other_recluck).not_to be_valid
+    end
+  end
 end

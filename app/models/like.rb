96dacbd49da87c940b70e534efe28465821d9c05
Jason Swett <jason@benfranklinlabs.com>
2020-05-17 10:19:29 -0400

    Add uniqueness validation for like.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :cluck
   belongs_to :user
+  validates :cluck, uniqueness: :user
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'trying to like the same cluck twice' do
+    it 'is not allowed' do
+      first_like = FactoryBot.create(:like)
+
+      other_like = FactoryBot.build(
+        :like,
+        cluck: first_like.cluck,
+        user: first_like.user
+      )
+
+      expect(other_like).not_to be_valid
+    end
+  end
 end

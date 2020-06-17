71c08ac7f64f11ced65d4e67d62af9bad946f9a3
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-17 10:40:29 -0600

    test for only one recluck for the same cluck

--- spec/models/recluck_spec.rb
+++ spec/models/recluck_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Recluck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can only recluck ones' do
+    recluck = FactoryBot.create(:recluck)
+    r = Recluck.new(user: recluck.user, cluck: recluck.cluck)
+    expect(r).to_not be_valid
+  end
+
+  it 'can recluck as the same user' do
+    recluck = FactoryBot.create(:recluck)
+    r = Recluck.new(user: recluck.user, cluck: FactoryBot.create(:cluck))
+    expect(r).to be_valid
+  end
 end

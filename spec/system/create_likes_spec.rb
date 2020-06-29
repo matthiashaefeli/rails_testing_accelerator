d4772e6bea656e71f87d8d5a0c7a7796396e5063
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-29 07:17:56 -0600

    do not show like if not logged in

--- spec/system/create_likes_spec.rb
+++ spec/system/create_likes_spec.rb
@@ -1,30 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Liking', type: :system do
-  before do
-    @me = FactoryBot.create(:user)
-    login_as(@me)
-  end
+  context 'user is logged in' do
+    before do
+      @me = FactoryBot.create(:user)
+      login_as(@me)
+    end
 
-  context 'cluck has not been liked' do
-    it 'can be liked' do
-      other_user = FactoryBot.create(:user)
-      FactoryBot.create(:cluck, user: other_user)
+    context 'cluck has not been liked' do
+      it 'can be liked' do
+        other_user = FactoryBot.create(:user)
+        FactoryBot.create(:cluck, user: other_user)
 
-      visit user_clucks_path(other_user)
-      expect(page).not_to have_content('1 like')
-      click_on 'Like'
-      expect(page).to have_content('1 like')
+        visit user_clucks_path(other_user)
+        expect(page).not_to have_content('1 like')
+        click_on 'Like'
+        expect(page).to have_content('1 like')
+      end
+    end
+
+    context 'cluck has already been liked' do
+      it 'does not have a like link' do
+        other_user = FactoryBot.create(:user)
+        cluck = FactoryBot.create(:cluck, user: other_user)
+        FactoryBot.create(:like, cluck: cluck, user: @me)
+
+        visit user_clucks_path(other_user)
+        expect(page).not_to have_content('Like')
+      end
     end
   end
 
-  context 'cluck has already been liked' do
-    it 'does not have a like link' do
-      other_user = FactoryBot.create(:user)
-      cluck = FactoryBot.create(:cluck, user: other_user)
-      FactoryBot.create(:like, cluck: cluck, user: @me)
-
-      visit user_clucks_path(other_user)
+  context 'user is not logged in' do
+    it 'does not show like if not login' do
+      cluck = FactoryBot.create(:cluck)
+      visit user_clucks_path(cluck.user)
       expect(page).not_to have_content('Like')
     end
   end
